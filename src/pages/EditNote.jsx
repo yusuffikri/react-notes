3020aa412a78b77543bf1a22ef373892a81ffa5d
yusuffikri <yusuffiks99@gmail.com>
2023-01-21 07:04:22 +0800

    edit note design

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import {IoIosArrowBack} from "react-icons/io"
+import {RiDeleteBin6Line} from "react-icons/ri"
 
 const EditNote = () => {
   return (
-    <div>EditNote</div>
+<section>
+  <header className="create-note__header">
+    <Link to="/" className="btn"><IoIosArrowBack/></Link>
+    <button className="btn lg primary">Save</button>
+    <button className="btn danger"><RiDeleteBin6Line/></button>
+  </header>
+  <form  className="create-note__form">
+    <input type="text" placeholder="Title" autoFocus />
+    <textarea rows="28" placeholder="Note details..."></textarea>
+  </form>
+</section>
   )
 }
 
