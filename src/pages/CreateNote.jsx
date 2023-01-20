26d6e74da02d7610fdd11d5ae56fe57cd9797bbf
yusuffikri <yusuffiks99@gmail.com>
2023-01-21 06:55:07 +0800

    create note design

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import {IoIosArrowBack} from "react-icons/io"
 
 const CreateNote = () => {
   return (
-    <div>CreateNote</div>
+<section>
+  <header className="create-note__header">
+    <Link to="/" className="btn"><IoIosArrowBack/></Link>
+    <button className="btn lg primary">Save</button>
+  </header>
+  <form  className="create-note__form">
+    <input type="text" placeholder="Title" autoFocus />
+    <textarea rows="28" placeholder="Note details..."></textarea>
+  </form>
+</section>
   )
 }
 
