284cbfcd1be22b251ffcf2937bb0c2d9c7e2329f
yusuffikri <yusuffiks99@gmail.com>
2023-01-20 19:48:40 +0800

    notes design update

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {BsSearch} from 'react-icons/bs'
 
 const Notes = () => {
   return (
-    <div>Notes</div>
+    <section>
+    <header className="notes__header">
+      <h2>My Notes</h2>
+      <input type="text" autoFocus placeholder="Keyword..." />
+      <button className="btn"><BsSearch/></button>
+    </header>
+    <div className="notes__container"></div>
+    </section>
   )
 }
 
