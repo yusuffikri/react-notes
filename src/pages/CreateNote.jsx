797a1d56d6017ab3f3cd3c15ef0629aa8284e1e9
yusuffikri <yusuffiks99@gmail.com>
2023-01-23 09:11:23 +0800

    create not function update

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,18 +1,28 @@
 import { Link } from "react-router-dom"
 import {IoIosArrowBack} from "react-icons/io"
 import { useState } from "react";
+import {v4 as uuid} from 'uuid';
 
 const CreateNote = () => {
   const [title,setTitle] = useState('');
   const [details,setDetails] = useState('');
 
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+
+    if (title && details ){
+    const note = {id: uuid(), title, details}
+    console.log(note)
+    }
+  } 
+
   return (
 <section>
   <header className="create-note__header">
     <Link to="/" className="btn"><IoIosArrowBack/></Link>
-    <button className="btn lg primary">Save</button>
+    <button className="btn lg primary" onClick={handleSubmit}>Save</button>
   </header>
-  <form  className="create-note__form">
+  <form  className="create-note__form" onSubmit={handleSubmit}>
     <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle (e.target.value)} autoFocus />
     <textarea rows="28" placeholder="Note details..." value={details} onChange={(e) => setDetails (e.target.value)}  ></textarea>
   </form>
