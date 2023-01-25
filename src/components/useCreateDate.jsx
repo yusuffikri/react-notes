cff989bf71d0775f58da06cba8dfe8092ddca4cb
yusuffikri <yusuffiks99@gmail.com>
2023-01-25 21:31:37 +0800

    create UseCreateDate

---
+++ src/components/useCreateDate.jsx
@@ -0,0 +1,36 @@
+const useCreateDate = () => {
+    const dateObj = new Date();
+    const month = dateObj.getMonth();
+    let monthName;
+    // eslint-disable-next-line default-case
+    switch(month){
+        case 0: monthName = "Jan";
+        break;
+        case 1: monthName = "Feb";
+        break;
+        case 2: monthName = "Mar";
+        break;
+        case 3: monthName = "Apr";
+        break;
+        case 4: monthName = "May";
+        break;
+        case 5: monthName = "Jun";
+        break;
+        case 6: monthName = "Jul";
+        break;
+        case 7: monthName = "Aug";
+        break;
+        case 8: monthName = "Sep";
+        break;
+        case 9: monthName = "Oct";
+        break;
+        case 10: monthName = "Nov";
+        break;
+        case 11: monthName = "Dec";
+        break;
+    }
+
+    const date = "${monthName} ${dateObj.getDate()}, ${dateObj.getFullYear()} [$(getDateO)]"
+}
+
+export default useCreateDate;
