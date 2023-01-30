40a3b4a7e18713b7268cb0fe9eff759f4b103d0d
yusuffikri <yusuffiks99@gmail.com>
2023-01-30 23:16:52 +0800

    adding handleForm

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -36,10 +36,10 @@ const EditNote = ({notes, setNotes}) => {
 <section>
   <header className="create-note__header">
     <Link to="/" className="btn"><IoIosArrowBack/></Link>
-    <button className="btn lg primary">Save</button>
+    <button className="btn lg primary" onClick={handleForm}>Save</button>
     <button className="btn danger"><RiDeleteBin6Line/></button>
   </header>
-  <form  className="create-note__form">
+  <form  className="create-note__form" onSubmit={handleForm}>
     <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} autoFocus />
     <textarea rows="28" placeholder="Note details..." value={details} onChange={(e) => setDetails(e.target.value)}></textarea>
   </form>
