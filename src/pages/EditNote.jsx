862029bbf500ecde93a5c3ed513f76566d579630
yusuffikri <yusuffiks99@gmail.com>
2023-01-30 23:12:38 +0800

    edit note functionality

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,14 +1,36 @@
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams, useNavigate } from "react-router-dom"
 import {IoIosArrowBack} from "react-icons/io"
 import {RiDeleteBin6Line} from "react-icons/ri"
 import { useState } from "react";
+import useCreateDate from "../components/useCreateDate";
 
 const EditNote = ({notes, setNotes}) => {
 
   const {id} = useParams();
-  const note = notes.find((item) => item.id == id);
+  const note = notes.find((item) => item.id === id);
   const [title, setTitle] = useState(note.title);
   const [details, setDetails] = useState(note.details);
+  const date = useCreateDate;
+  const navigate = useNavigate;
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    if (title && details) {
+      const newNote = {...note, title, details, date}
+
+      const newNotes = notes.map(item => {
+        if(item.id === id) {
+          item = newNote;
+        }
+        return item;
+      })
+
+      setNotes = newNotes;
+    }
+
+    navigate('/')
+  }
 
   return (
 <section>
@@ -18,8 +40,8 @@ const EditNote = ({notes, setNotes}) => {
     <button className="btn danger"><RiDeleteBin6Line/></button>
   </header>
   <form  className="create-note__form">
-    <input type="text" placeholder="Title" autoFocus />
-    <textarea rows="28" placeholder="Note details..."></textarea>
+    <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} autoFocus />
+    <textarea rows="28" placeholder="Note details..." value={details} onChange={(e) => setDetails(e.target.value)}></textarea>
   </form>
 </section>
   )
