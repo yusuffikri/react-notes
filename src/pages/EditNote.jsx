e986eee957cedcbc09ccf3da14a9c61b8d1a587b
yusuffikri <yusuffiks99@gmail.com>
2023-02-04 14:16:31 +0800

    update delete notes

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -28,8 +28,15 @@ const EditNote = ({notes, setNotes}) => {
 
       setNotes = newNotes;
     }
-
+    // redirect to homepage
     navigate('/')
+  }
+
+  const handleDelete = () => {
+    const newNotes = notes.filter(item => item.id !== id);
+
+    setNotes(newNotes);
+    navigate('/');
   }
 
   return (
@@ -37,7 +44,7 @@ const EditNote = ({notes, setNotes}) => {
   <header className="create-note__header">
     <Link to="/" className="btn"><IoIosArrowBack/></Link>
     <button className="btn lg primary" onClick={handleForm}>Save</button>
-    <button className="btn danger"><RiDeleteBin6Line/></button>
+    <button className="btn danger" onClick={handleDelete}><RiDeleteBin6Line/></button>
   </header>
   <form  className="create-note__form" onSubmit={handleForm}>
     <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} autoFocus />
